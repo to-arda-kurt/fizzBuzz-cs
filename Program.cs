3e6f8f6dc674e6384c2e6ede18b6d7d5f3722d31
Arda Kurt <to.arda.kurt@gmail.com>
2023-08-07 11:45:00 +0100

    FizBuzz Initial

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        for (var i = 1; i <= 100; i++)
+        {
+            bool dividiblebyThree = IsDividible(i, 3);
+            bool dividiblebyFive = IsDividible(i, 5);
+            string message = "";
+
+            if (dividiblebyThree)
+            {
+                message = message + "Fizz";
+            }
+
+            if (dividiblebyFive)
+            {
+                message = message + "Buzz";
+            }
+
+            if (message.Length == 0)
+            {
+                message = i.ToString();
+
+            }
+
+            Console.WriteLine(message);
+        }
+
+
+        static bool IsDividible(int fizzBuzzNumber, int divider)
+        {
+            return (fizzBuzzNumber % divider) == 0;
+        }
+    }
+}
